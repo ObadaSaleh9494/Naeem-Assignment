2860567df3d199d0646f03d801e6891c0bbbaa8b
Obada Saleh <obada.saleh.9494@gmail.com>
2023-01-18 13:11:48 +0200

    add apexchart

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
     <the-header></the-header>
+    <apexchart
+      width="500"
+      type="bar"
+      :options="options"
+      :series="series"
+    ></apexchart>
   </v-app>
 </template>
 
@@ -12,9 +18,21 @@ export default {
   components: {
     TheHeader,
   },
-
-  data: () => ({
-    //
-  }),
+  data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
 
+Vue.component('apexchart', VueApexCharts)
 new Vue({
   vuetify,
   render: h => h(App)
