b7e92484fd041fc0bface66ff8084d5ddf87eb4c
Obada Saleh <obada.saleh.9494@gmail.com>
2023-01-18 11:29:41 +0200

    the header

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="white" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2 mb-2"
+        contain
+        src="https://naeem.app/wp-content/uploads/2020/10/%D9%85%D8%AE%D8%AE%D9%842-1.png"
+        transition="scale-transition"
+        width="150"
+      />
+    </div>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
